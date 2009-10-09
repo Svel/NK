bb24465775f2564f45f241a517a26a6501802e35
Svel <Svel.Sontz@gmail.com>
2009-10-09 11:01:44 +0400

    #15 - список последних добавленных объявлений

--- lib/model/doctrine/PostTable.class.php
+++ lib/model/doctrine/PostTable.class.php
@@ -9,6 +9,11 @@ class PostTable extends Doctrine_Table
        ->where('p.expires_at > ?', date('Y-m-d H:i:s', time()))
        ->orderBy('p.created_at DESC')
        ->limit(sfConfig::get('app_posts_home_page_limit', 10));
+
+    if (sfConfig::get('app_posts_home_page_validated', false)) {
+      $q->addWhere('p.is_validated = ?', true);
+    }
+
     return $q->execute();
   }
 }
